5f08c86930554c68836c7d0089c11b5d19a22304
Luc Deschenaux <luc.deschenaux@foxel.ch>
2014-10-09 20:25:06 +0200

    add include for opencv backward compatibility

--- src/gnoproj.hpp
+++ src/gnoproj.hpp
@@ -61,6 +61,7 @@
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgcodecs/imgcodecs_c.h>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
