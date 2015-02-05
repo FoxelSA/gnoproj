bb6d40d883e514666d0a439543564e379d6001c3
sflotron <s.flotron@foxel.ch>
2015-02-05 13:43:07 +0100

    more cleaning

--- src/gnoproj.cpp
+++ src/gnoproj.cpp
@@ -48,11 +48,12 @@
 using namespace std;
 using namespace cv;
 
-/*********************************************************************
- * Split an input string with a delimiter and fill a string vector
- *
- *********************************************************************
- */
+/******************************************************************************
+*
+*   main function
+*
+*******************************************************************************
+*/
 
 int main(int argc, char** argv) {
 
@@ -68,7 +69,6 @@ int main(int argc, char** argv) {
     std::string mac_address(argv[2]);  //mac adress
     std::string mount_point(argv[3]);  // mount point
     std::string input_image(input_image_filename);
-    std::string output_image_filename; // output image filename
 
     // check is a focal length is given, and update method if necessary
     int  normalizedFocal(0);  // gnomonic projection method. 0 elphel method (default), 1 with constant focal
@@ -92,98 +92,14 @@ int main(int argc, char** argv) {
       }
     }
 
-    // extract channel information from image name
-    std::vector<string>  splitted_name;
-    std::vector<string>  out_split;
+    // do gnomonic projection
+    const bool  bProjected = eqrToGnomonic (
+          input_image,
+          mount_point,
+          mac_address,
+          normalizedFocal,
+          focal
+    );
 
-    split( input_image, "-", splitted_name );
-    split( input_image, "_", out_split );
-
-    const size_t sensor_index=atoi(splitted_name[1].c_str());
-    sensorData   sensorSD;
-
-    // load calibration informations
-    bool  bLoadCalibration = loadCalibrationData
-                   ( sensorSD,
-                     sensor_index,
-                     mount_point,
-                     mac_address );
-
-    if( !bLoadCalibration )
-    {
-        std::cerr << " Failed to load calibration informations. Exit " << std::endl;
-        return -1;
-    }
-
-    // load image
-    IplImage* eqr_img = cvLoadImage(input_image_filename, CV_LOAD_IMAGE_COLOR );
-
-    /* Initialize output image structure */
-    IplImage* out_img = cvCreateImage( cvSize( sensorSD.lfWidth, sensorSD.lfHeight ), IPL_DEPTH_8U , eqr_img->nChannels );
-
-    if(!normalizedFocal){
-        /* Gnomonic projection of the equirectangular tile */
-        lg_ttg_elphel(
-            ( inter_C8_t *) eqr_img->imageData,
-            eqr_img->width,
-            eqr_img->height,
-            eqr_img->nChannels,
-            ( inter_C8_t *) out_img->imageData,
-            out_img->width,
-            out_img->height,
-            out_img->nChannels,
-            sensorSD.lfpx0,
-            sensorSD.lfpy0,
-            sensorSD.lfImageFullWidth,
-            sensorSD.lfImageFullHeight-1, // there's an extra pixel for wrapping
-            sensorSD.lfXPosition,
-            sensorSD.lfYPosition,
-            sensorSD.lfRoll,
-            sensorSD.lfAzimuth,
-            sensorSD.lfElevation,
-            sensorSD.lfHeading,
-            sensorSD.lfPixelSize,
-            sensorSD.lfFocalLength,
-            li_bicubicf
-        );
-
-        // create output image name
-        output_image_filename+=out_split[0]+"_"+out_split[1]+"-RECT-SENSOR.tiff";
-    }
-    else
-    {
-      /* Gnomonic projection of the equirectangular tile */
-       lg_ttg_center(
-          ( inter_C8_t *) eqr_img->imageData,
-          eqr_img->width,
-          eqr_img->height,
-          eqr_img->nChannels,
-          ( inter_C8_t *) out_img->imageData,
-          out_img->width,
-          out_img->height,
-          out_img->nChannels,
-          sensorSD.lfImageFullWidth,
-          sensorSD.lfImageFullHeight-1,
-          sensorSD.lfXPosition,
-          sensorSD.lfYPosition,
-          sensorSD.lfAzimuth + sensorSD.lfHeading + LG_PI,
-          sensorSD.lfElevation,
-          sensorSD.lfRoll,
-          focal,
-          sensorSD.lfPixelSize,
-          li_bicubicf
-        );
-
-        // create output image name
-        output_image_filename+=out_split[0]+out_split[1]+"-RECT-CONFOC.tiff";
-    }
-
-    /* Gnomonic image exportation */
-    cvSaveImage(output_image_filename.c_str() , out_img, NULL );
-
-    /* Free memory */
-    cvReleaseImage(&eqr_img);
-    cvReleaseImage(&out_img);
-
-    return 0;
+    return !bProjected;
 }
--- src/gnoproj.hpp
+++ src/gnoproj.hpp
@@ -62,4 +62,30 @@
 
 #define DEBUG 0
 
+/*********************************************************************
+* Split an input string with a delimiter and fill a string vector
+*
+*********************************************************************
+*/
+
+static bool split ( const std::string src, const std::string& delim, std::vector<std::string>& vec_value )
+{
+  bool bDelimiterExist = false;
+  if ( !delim.empty() )
+  {
+    vec_value.clear();
+    std::string::size_type start = 0;
+    std::string::size_type end = std::string::npos -1;
+    while ( end != std::string::npos )
+    {
+      end = src.find ( delim, start );
+      vec_value.push_back ( src.substr ( start, end - start ) );
+      start = end + delim.size();
+    }
+    if ( vec_value.size() >= 2 )
+      bDelimiterExist = true;
+    }
+    return bDelimiterExist;
+  }
+
 #endif /* GNOPROJ_HPP_ */
