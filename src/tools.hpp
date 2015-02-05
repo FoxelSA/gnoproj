5ef1cb3b9b65776bb78294c5221eadae304792c2
sflotron <s.flotron@foxel.ch>
2015-02-05 14:19:06 +0100

    begin to add doxygen documention

--- src/tools.hpp
+++ src/tools.hpp
@@ -40,6 +40,40 @@
 *      Attribution" section of <http://foxel.ch/license>.
 */
 
+  /*! \file tools.hpp
+   * \author Stephane Flotron <s.flotron@foxel.ch>
+   *
+   * Software main header
+   */
+  /*! \mainpage gnoproj
+   * \section gnoproj
+   *
+   * Gnomonic reprojection software for equirectangular images from ElphelPHG cameras
+   *
+   * \section Documentation
+   *
+   * Documentation can be consulted on the [wiki](https://github.com/FoxelSA/gnoproj/wiki).
+   *
+   * \section Copyright
+   *
+   * Copyright (c) 2013-2014 FOXEL SA - [http://foxel.ch](http://foxel.ch)<br />
+   * This program is part of the FOXEL project <[http://foxel.ch](http://foxel.ch)>.
+   *
+   * Please read the [COPYRIGHT.md](COPYRIGHT.md) file for more information.
+   *
+   * \section License
+   *
+   * This program is licensed under the terms of the
+   * [GNU Affero General Public License v3](http://www.gnu.org/licenses/agpl.html)
+   * (GNU AGPL), with two additional terms. The content is licensed under the terms
+   * of the [Creative Commons Attribution-ShareAlike 4.0 International](http://creativecommons.org/licenses/by-sa/4.0/)
+   * (CC BY-SA) license.
+   *
+   * You must read <[http://foxel.ch/license](http://foxel.ch/license)> for more
+   *information about our Licensing terms and our Usage and Attribution guidelines.
+   *
+   */
+
 #ifndef TOOLS_HPP_
 #define TOOLS_HPP_
 
@@ -52,6 +86,19 @@ using namespace cv;
 /******************************************************************************
 * sensorData
 *****************************************************************************/
+
+/*! \struct sensor Data
+* \brief structure used to store calibration information
+*
+* This structure is designed to store the needed informations coming from
+* the elphel camera calibration
+*
+* \var sensorData::lfWidth
+*  Width of sensor image
+* \var sensorData::lfHeight
+*  Height of sensor image
+*/
+
 struct sensorData
 {
   lf_Size_t   lfWidth     = 0;
@@ -158,7 +205,7 @@ bool  loadCalibrationData( sensorData & sD,
         std::cerr << " Could not read calibration data. " << std::endl;
         return false;
     }
-    
+
 };
 
 /*********************************************************************
