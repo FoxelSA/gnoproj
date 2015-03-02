d746bc3b0378e4ad0ab3a7cef3caece359ed3580
sflotron <s.flotron@foxel.ch>
2015-03-02 16:48:42 +0100

    updated scripts

--- scripts/gnoproj.sh
+++ scripts/gnoproj.sh
@@ -1,5 +1,5 @@
   #!/bin/sh
-  # $1 eqr image, $2 output directory $3 mac address  $4 mount point 
+  # $1 eqr image, $2 output directory $3 mac address  $4 mount point
   # project EQR using gnoproj
-  /home/sflotron/foxel/git/gnoproj/build/gnoproj $1 $2 $3 $4
-  #/home/foxel/src/gnoproj/build/gnoproj $1 $2 $3 9
+  /home/foxel/src/gnoproj/build/gnoproj -i $1 -o $2 -m $3 -d $4
+  #/home/foxel/src/gnoproj/build/gnoproj -i $1 -o $2 -m $3 -d $4 -f 9
